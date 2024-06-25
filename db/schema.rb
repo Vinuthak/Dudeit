59a5bff27d0554f7e6e81c5eb979bd496a17bca3
Vinuthak <vinutha.kasaragod@gmail.com>
2024-06-25 21:42:16 +0200

    Add has_many and belongs_to relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_091246) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_192503) do
   create_table "stories", force: :cascade do |t|
     t.string "name"
     t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "story_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
