610721c2c234ed1c4780dbee7bc1329b45d40c1f
Vinuthak <vinutha.kasaragod@gmail.com>
2024-07-17 22:33:48 +0200

    Add controller tests for the show page

--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -51,4 +51,17 @@ class StoriesControllerTest < ActionDispatch::IntegrationTest
       }
     end
   end
+
+  test "show story" do
+    get story_path(stories(:one))
+    assert_response :success
+    assert response.body.include?(stories(:one).name)
+  end
+
+  test "story vote elements" do
+    get story_path(stories(:one))
+    assert_select 'h2 span#vote_score'
+    assert_select 'ul#vote_history li', count: 3
+    assert_select 'div #vote_form form'
+  end
 end
